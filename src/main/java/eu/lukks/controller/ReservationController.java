8885afca436576a4c6294cd1507df3874d5e0646
lukasz.dela <lukasz.dela@lukks.eu>
2018-12-05 20:46:39 +0100

    basics add, update, delete functionality created

--- src/main/java/eu/lukks/controller/ReservationController.java
+++ src/main/java/eu/lukks/controller/ReservationController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import eu.lukks.domain.Reservation;
@@ -32,12 +33,43 @@ public String getAllReservations(Model model) {
 
 
 @PostMapping("/reservation/add")
-public String newReservation(@ModelAttribute Reservation reservation) {
+public String newReservation(@ModelAttribute Reservation reservation,
+								Model model) {
 	iReservationService.saveReservation(reservation);
+	List<Reservation> reservations = iReservationService.findAllReservations();
+	model.addAttribute("reservations", reservations);
 	return "index";
 }
 	
-	
-	
-	
+@GetMapping("/reservation/delete/{reservationId}")
+public String deleteReservationById(@PathVariable("reservationId")Long reservationId,
+													Model model) {
+	iReservationService.deleteReservationById(reservationId);
+	List<Reservation> reservationList = iReservationService.findAllReservations();
+	model.addAttribute("reservations", reservationList);
+	return "index";
+}
+
+@PostMapping("/reservation/update/save/{reservationId}")
+public String updateReservation(@ModelAttribute Reservation reservation,
+								@PathVariable("reservationId")Long reservationId,
+								Model model) {
+Reservation reservationById = iReservationService.readReservationById(reservationId);
+reservationById.setName(reservation.getName());
+reservationById.setDate(reservation.getDate());
+iReservationService.saveReservation(reservationById);
+List<Reservation> reservations = iReservationService.findAllReservations();
+model.addAttribute("reservations", reservations);
+return "index";
+}
+
+@PostMapping("/reservation/update/{reservationId}")
+public String updateReservationSite(@PathVariable("reservationId")Long reservationId,
+									Model model) {
+	Reservation reservation = iReservationService.readReservationById(reservationId);
+	model.addAttribute("reservation", reservation);
+	return "update";
+}
+
+
 }
